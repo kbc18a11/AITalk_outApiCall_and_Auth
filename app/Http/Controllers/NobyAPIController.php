959430670d60511a3d44768aee50b731511b64d9
Naoumi1214 <wada121400@gmail.com>
2020-05-22 15:58:07 +0900

    update : リクエストの処理をモデルに移した（バリエーションの実装はまだ）

--- app/Http/Controllers/NobyAPIController.php
+++ app/Http/Controllers/NobyAPIController.php
@@ -3,33 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
+use App\NobyAPI;
 
 class NobyAPIController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
+     *　APIサービスにリクエスト
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //URL
-        $nobyApiUrl = 'https://app.cotogoto.ai';
+        $nobyApi = new NobyAPI();
+        //APIにリクエスト
+        $res = $nobyApi->apiReqest($request->text);
 
-        //Clientインスタンス化
-        $client = new Client([
-            'base_uri' => $nobyApiUrl,
-        ]);
-
-        $path = '/webapi/noby.json?appkey=ca6bb3e3a9a11cf0ba890cd350054eb8&mail=wada121400@gmail.com&pass=Tabunnsoreha07-&text=今日も1日頑張りましょう。&lat=&lng=&study=&persona=&ending=';
-        $res = $client->request(
-            'GET',
-            $path,
-            [
-                'allow_redirects' => true,
-            ]
-        );
 
         return response(json_decode($res->getBody()->getContents(), true));
     }
--- app/NobyAPI.php
+++ app/NobyAPI.php
@@ -8,21 +8,26 @@ use GuzzleHttp\Client;
 class NobyAPI extends Model
 {
     //APIのURL
-    private $nobyApiUrl = 'https://app.cotogoto.ai';
+    private static $nobyApiUrl = 'https://app.cotogoto.ai';
 
-
-
-    public function apiReqest()
+    /**
+     * APIにリクエストして、結果を返す
+     * @param string $text
+     */
+    public function apiReqest(string $text)
     {
         # code...
-        //URL
+
 
         //Clientインスタンス化
         $client = new Client([
             'base_uri' => $this->nobyApiUrl,
         ]);
 
-        $path = '/webapi/noby.json?appkey=ca6bb3e3a9a11cf0ba890cd350054eb8&mail=wada121400@gmail.com&pass=Tabunnsoreha07-&text=今日も1日頑張りましょう。&lat=&lng=&study=&persona=&ending=';
+        //リクエストパラメータを含んだURL
+        $path = 'https://app.cotogoto.ai/webapi/noby.json?appkey=' . env('NODY_API_APPKEY') . '&mail=' . env('NODY_API_MAIL') . '&pass=' . env('NODY_API_PASS') . '&text=' . $text . '&lat=&lng=&study=&persona=&ending=';
+
+        //リクエスト開始
         $res = $client->request(
             'GET',
             $path,
@@ -30,5 +35,7 @@ class NobyAPI extends Model
                 'allow_redirects' => true,
             ]
         );
+
+        return $res;
     }
 }
