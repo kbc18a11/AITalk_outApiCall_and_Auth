61b8f33a6337852567ce6053c3e9f4c27933e471
Naoumi1214 <wada121400@gmail.com>
2020-05-29 15:48:54 +0900

    add : Amazon Polly関係のコントローラーとモデル、マイグレーションを生成

---
+++ database/factories/AmazonPollyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\AmazonPolly;
+use Faker\Generator as Faker;
+
+$factory->define(AmazonPolly::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
