c6c54505ac8f93f1d331d7326e604450939b971c
Naoumi1214 <wada121400@gmail.com>
2020-06-19 16:09:09 +0900

    update : パスワードをhash化させた

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class UserSeeder extends Seeder
                 'name' => 'テスト太郎',
                 'email' => 'test@test.com',
                 'icon' => 'https://aitoke.s3-ap-northeast-1.amazonaws.com/icon/default/default_icon.png',
-                'password' => 'testpassword'
+                'password' => Hash::make('testpassword')
             ]
         ];
         DB::table('users')->insert($users);
