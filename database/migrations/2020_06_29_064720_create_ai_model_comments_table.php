4b27cbdb3a9fd89bb81722522d816df893bec9c1
kbc18a11 <wada121400@gmail.com>
2020-06-29 15:48:18 +0900

    add : 複数ぼマイグレーションを追加

---
+++ database/migrations/2020_06_29_064720_create_ai_model_comments_table.php
@@ -0,0 +1,54 @@
+
+    <?php
+        use Illuminate\Support\Facades\Schema;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Database\Migrations\Migration;
+        
+        class CreateAiModelCommentsTable extends Migration
+        {
+            /**
+             * Run the migrations.
+             *
+             * @return void
+             */
+            public function up()
+            {
+                Schema::create("ai_model_comments", function (Blueprint $table) {
+
+						$table->increments('id');
+						$table->integer('ai_model_is')->unsigned();
+						$table->integer('user_id')->unsigned();
+						$table->string('comment');
+						$table->timestamps();
+						$table->softDeletes();
+						
+						//$table->foreign("ai_model_is")->references("id")->on("ai_models");
+						//$table->foreign("user_id")->references("id")->on("users");
+
+
+
+						// ----------------------------------------------------
+						// -- SELECT [ai_model_comments]--
+						// ----------------------------------------------------
+						// $query = DB::table("ai_model_comments")
+						// ->leftJoin("ai_models","ai_models.id", "=", "ai_model_comments.ai_model_is")
+						// ->leftJoin("users","users.id", "=", "ai_model_comments.user_id")
+						// ->get();
+						// dd($query); //For checking
+
+
+
+                });
+            }
+
+            /**
+             * Reverse the migrations.
+             *
+             * @return void
+             */
+            public function down()
+            {
+                Schema::dropIfExists("ai_model_comments");
+            }
+        }
+    
---
+++ database/migrations/2020_06_29_064720_create_ai_models_table.php
@@ -0,0 +1,59 @@
+
+    <?php
+        use Illuminate\Support\Facades\Schema;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Database\Migrations\Migration;
+        
+        class CreateAiModelsTable extends Migration
+        {
+            /**
+             * Run the migrations.
+             *
+             * @return void
+             */
+            public function up()
+            {
+                Schema::create("ai_models", function (Blueprint $table) {
+
+						$table->increments('id');
+						$table->bigInteger('user_id')->unsigned();
+						$table->string('name');
+						$table->string('self_introduction')->nullable(); //自己紹介文
+						$table->string('opne_mouth_image'); //口が開いている顔
+						$table->string('close_mouth_image'); //口が閉じている顔
+						$table->timestamps();
+						$table->softDeletes();
+						
+						
+
+                    //*********************************
+                    // Foreign KEY [ Uncomment if you want to use!! ]
+                    //*********************************
+                        //$table->foreign("user_id")->references("id")->on("users");
+
+
+
+						// ----------------------------------------------------
+						// -- SELECT [ai_models]--
+						// ----------------------------------------------------
+						// $query = DB::table("ai_models")
+						// ->leftJoin("users","users.id", "=", "ai_models.user_id")
+						// ->get();
+						// dd($query); //For checking
+
+
+
+                });
+            }
+
+            /**
+             * Reverse the migrations.
+             *
+             * @return void
+             */
+            public function down()
+            {
+                Schema::dropIfExists("ai_models");
+            }
+        }
+    
