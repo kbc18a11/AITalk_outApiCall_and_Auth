239f29d80f880ff67758e866788e06757c1430f6
kbc18a11 <wada121400@gmail.com>
2020-07-20 15:08:34 +0900

    update : バリエーションとバリエーションエラーメッセージの実装が完了した #3 #4

--- app/AiModel.php
+++ app/AiModel.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class AiModel extends Model
 {
@@ -12,10 +13,35 @@ class AiModel extends Model
      * @var array
      */
     private static $createRules = [
-        'user_id' => ['required', 'exists:users,id'],
+        'user_id' => ['required', 'integer', 'exists:users,id'],
         'name' => ['required', 'string', 'max:255', 'unique:ai_models'],
         'self_introduction' => ['string', 'max:255'],
         'open_mouth_image' => ['required', 'image'],
         'close_mouth_image' => ['required', 'image']
     ];
+
+    /**
+     * エラーメッセージ一覧
+     * @var array
+     */
+    private static $errorMessages = [
+        'required' => '必須項目です。',
+        'integer' => '数値を入力してください',
+        'string' => '文字を入力してください',
+        'max' => '255文字以下入力してください',
+        'exists' => 'ユーザーが存在しません',
+        'unique' => '既にほかのキャラクターが利用しています',
+        'image' => '画像を指定してください'
+    ];
+
+    /**
+     * ユーザー登録のパラメータのバリデーションの検証
+     * @param array
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    public static function createValidator(array $array)
+    {
+        # code...
+        return Validator::make($array, self::$createRules, self::$errorMessages);
+    }
 }
--- app/Http/Controllers/AiModelsController.php
+++ app/Http/Controllers/AiModelsController.php
@@ -18,30 +18,31 @@ class AiModelsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        //バリデーションの検証
+        $validationResult = AiModel::createValidator($request->all());
+        //バリデーションの結果が駄目か？
+        if ($validationResult->fails()) {
+            # code...
+            return response()->json([
+                'createResult' => false,
+                'error' => $validationResult->messages()
+            ], 422);
+        }
+
+        return response()->json(['aa' => 'aa']);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\AiModel  $aiModel
+     * @param \App\AiModel $aiModel
      * @return \Illuminate\Http\Response
      */
     public function show(AiModel $aiModel)
@@ -50,32 +51,22 @@ class AiModelsController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\AiModel  $aiModel
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(AiModel $aiModel)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\AiModel  $aiModel
+     * @param \Illuminate\Http\Request $request
+     * @param \App\AiModel $aiModel
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, AiModel $aiModel)
     {
         //
+        return response()->json(['aa' => 'aa']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\AiModel  $aiModel
+     * @param \App\AiModel $aiModel
      * @return \Illuminate\Http\Response
      */
     public function destroy(AiModel $aiModel)
