03ffb218ae0087399b01551228d55f37ecb4c579
kbc18a11 <wada121400@gmail.com>
2020-08-15 20:04:14 +0900

    update : テーブルの構成を変更した

--- database/migrations/2020_06_29_064720_create_favorite_ai_models_table.php
+++ database/migrations/2020_06_29_064720_create_favorite_ai_models_table.php
@@ -1,51 +1,50 @@
+<?php
 
-    <?php
-        use Illuminate\Support\Facades\Schema;
-        use Illuminate\Database\Schema\Blueprint;
-        use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-        class CreateFavoriteAiModelsTable extends Migration
-        {
-            /**
-             * Run the migrations.
-             *
-             * @return void
-             */
-            public function up()
-            {
-                Schema::create("favorite_ai_models", function (Blueprint $table) {
+class CreateFavoriteAiModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("favorite_ai_models", function (Blueprint $table) {
+            $table->engine = 'InnoDB';
 
-						$table->increments('id');
-						$table->integer('user_id')->unsigned();
-						$table->integer('ai_model_id')->unsigned();
-						$table->timestamps();
+            $table->id();
+            $table->biginteger('user_id')->unsigned();
+            $table->biginteger('ai_model_id')->unsigned();
+            $table->timestamps();
 
-						//$table->foreign("user_id")->references("id")->on("users");
-						//$table->foreign("ai_model_id")->references("id")->on("ai_models");
-
-
-
-						// ----------------------------------------------------
-						// -- SELECT [favorite_ai_models]--
-						// ----------------------------------------------------
-						// $query = DB::table("favorite_ai_models")
-						// ->leftJoin("users","users.id", "=", "favorite_ai_models.user_id")
-						// ->leftJoin("ai_models","ai_models.id", "=", "favorite_ai_models.ai_model_id")
-						// ->get();
-						// dd($query); //For checking
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->foreign("ai_model_id")->references("id")->on("ai_models");
 
 
+            // ----------------------------------------------------
+            // -- SELECT [favorite_ai_models]--
+            // ----------------------------------------------------
+            // $query = DB::table("favorite_ai_models")
+            // ->leftJoin("users","users.id", "=", "favorite_ai_models.user_id")
+            // ->leftJoin("ai_models","ai_models.id", "=", "favorite_ai_models.ai_model_id")
+            // ->get();
+            // dd($query); //For checking
 
-                });
-            }
 
-            /**
-             * Reverse the migrations.
-             *
-             * @return void
-             */
-            public function down()
-            {
-                Schema::dropIfExists("favorite_ai_models");
-            }
-        }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("favorite_ai_models");
+    }
+}
--- database/migrations/2020_06_29_064720_create_follows_table.php
+++ database/migrations/2020_06_29_064720_create_follows_table.php
@@ -1,51 +1,50 @@
+<?php
 
-    <?php
-        use Illuminate\Support\Facades\Schema;
-        use Illuminate\Database\Schema\Blueprint;
-        use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-        class CreateFollowsTable extends Migration
-        {
-            /**
-             * Run the migrations.
-             *
-             * @return void
-             */
-            public function up()
-            {
-                Schema::create("follows", function (Blueprint $table) {
+class CreateFollowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("follows", function (Blueprint $table) {
+            $table->engine = 'InnoDB';
 
-						$table->increments('id');
-						$table->integer('user_id')->unsigned(); //フォローをしているユーザー
-						$table->integer('follow_user_id')->unsigned(); //フォローを受けるユーザー
-						$table->timestamps();
+            $table->id();
+            $table->biginteger('user_id')->unsigned(); //フォローをしているユーザー
+            $table->biginteger('follow_user_id')->unsigned(); //フォローを受けるユーザー
+            $table->timestamps();
 
-						//$table->foreign("user_id")->references("id")->on("users");
-						//$table->foreign("follow_user_id")->references("id")->on("users");
-
-
-
-						// ----------------------------------------------------
-						// -- SELECT [follows]--
-						// ----------------------------------------------------
-						// $query = DB::table("follows")
-						// ->leftJoin("users","users.id", "=", "follows.user_id")
-						// ->leftJoin("users","users.id", "=", "follows.follow_user_id")
-						// ->get();
-						// dd($query); //For checking
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->foreign("follow_user_id")->references("id")->on("users");
 
 
+            // ----------------------------------------------------
+            // -- SELECT [follows]--
+            // ----------------------------------------------------
+            // $query = DB::table("follows")
+            // ->leftJoin("users","users.id", "=", "follows.user_id")
+            // ->leftJoin("users","users.id", "=", "follows.follow_user_id")
+            // ->get();
+            // dd($query); //For checking
 
-                });
-            }
 
-            /**
-             * Reverse the migrations.
-             *
-             * @return void
-             */
-            public function down()
-            {
-                Schema::dropIfExists("follows");
-            }
-        }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("follows");
+    }
+}
